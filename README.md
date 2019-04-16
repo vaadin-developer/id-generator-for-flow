e61bd2a20d2da8f76fc261c00ece87ff2457fa0b
Sven Ruppert <sven.ruppert@gmail.com>
2019-04-16 16:54:39 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# id-generator-for-flow
-IDGenerator for Flow
+# IDGenerator for Flow
+If IDs for components are needed in a project, it makes sense to use an ID generator. 
+With this, you get in a project ID's that are always built according to the same scheme.
+Relevant here is that these numbers are kept in human-readable form. 
+This strategy facilitates the assignment of log messages to the particular source code point later during operation.
+
+Another requirement is the uniqueness of identification numbers. 
+With this, it is the only way to guarantee that the tests based on it work flawlessly.
+
+To use this ID generator now, you can use the static methods in the interface named **VaadinIDGenerator**. 
+The method **genericID** returns a generic and neutral ID generator. 
+This result is a function with three input parameters. 
+Based on these parameters, the ID will be generated.
+
+```java
+  static TriFunction<Class, Class, String, String> genericID() {
+    return (uiClass , componentClass , label)
+        -> (uiClass.getSimpleName()
+            + "-" + componentClass.getSimpleName()
+            + "-" + label.replace(" " , "-"))
+        .toLowerCase(Locale.US);
+  }
+```
+
+To take the next step, you can specialise the ID generators on the particular component types. 
+This refers to the components contained in Vaadin Flow, such as the button.
+
+```java
+  static Function<Class, BiFunction<Class, String, String>> typedComponentIDGenerator() {
+    return (clazz) -> (uiClass , label) -> genericID().apply(uiClass , clazz , label);
+  }
+  
+  static BiFunction<Class, String, String> buttonID() {
+    return typedComponentIDGenerator().apply(Button.class);
+  }
+```
+
+To use this ID generator, only the class which should hold the 
+particular element and a logical identifier which then leads to the 
+domain-specific name of the individual components is needed at the place of use.
+
+```java
+  public static final String BTN_CLICK_ME_ID = buttonID().apply(ClickCounterWorkspace.class, "btn-click-me");
+  public static final String LB_CLICK_COUNT_ID = spanID().apply(ClickCounterWorkspace.class, "lb-click-count");
+```
+
+But not only identification numbers benefit from this ID generator. 
+You can just as well use this method to generate unique keys for the I18n mechanism. 
+Here only a slightly modified structure is used.
+
+```java
+  static Function<String, String> caption() {
+    return (id) -> id + "." + "caption";
+  }
+
+  static Function<String, String> placeholder() {
+    return (id) -> id + "." + "placeholder";
+  }
+```
+
+## Available / typed IDGenerators
+* Vaadin Grid 
+* Vaadin Button 
+* Vaadin Combo Box
+* Vaadin Date Picker
+* Vaadin Label
+* Vaadin Span
+* Vaadin Text Field - Text / Password
+* Vaadin Checkbox
+* Vaadin Ordered Layout - Horizontal / Vertical
+* Vaadin Context Menu
+* Vaadin Time Picker
+* Vaadin Details 
+* Vaadin Dialog 
+* Vaadin Select
+* Vaadin Form Layout
+* Vaadin Icons
+* Vaadin List Box 
+* Vaadin Progress Bar 
+* Vaadin Radio Button
+* Vaadin Split Layout
+* Vaadin Tabs 
+* Vaadin Upload 
+* Vaadin Custom Field 
+
+* Vaadin Charts 
+* Vaadin Grid Pro 
+* Vaadin Rich Text Editor 
+* Vaadin Crud 
+* Vaadin Cookie Consent 
+* Vaadin Confirm Dialog 
+
+
+## Backlog
+* Vaadin Accordion
+* Vaadin Item 
+* Vaadin Notification 
+* Vaadin App Layout 
+* Vaadin Login 
+* Vaadin Board 
---
+++ id-generator-impl/src/main/java/org/rapidpm/vaadin/addon/idgenerator/VaadinIDGenerator.java
@@ -0,0 +1,212 @@
+/**
+ * Copyright © 2018 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rapidpm.vaadin.addon.idgenerator;
+
+import java.util.Locale;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.rapidpm.frp.functions.TriFunction;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.cookieconsent.CookieConsent;
+import com.vaadin.flow.component.crud.Crud;
+import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.gridpro.GridPro;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.listbox.ListBox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.richtexteditor.RichTextEditor;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.component.upload.Upload;
+
+public interface VaadinIDGenerator {
+
+  static TriFunction<Class, Class, String, String> genericID() {
+    return (uiClass , componentClass , label)
+        -> (uiClass.getSimpleName()
+            + "-" + componentClass.getSimpleName()
+            + "-" + label.replace(" " , "-"))
+        .toLowerCase(Locale.US);
+  }
+
+  static Function<Class, BiFunction<Class, String, String>> typedComponentIDGenerator() {
+    return (clazz) -> (uiClass , label) -> genericID().apply(uiClass , clazz , label);
+  }
+
+
+  static Function<String, String> caption() {
+    return (id) -> id + "." + "caption";
+  }
+
+  static Function<String, String> placeholder() {
+    return (id) -> id + "." + "placeholder";
+  }
+
+
+  static BiFunction<Class, String, String> gridID() {
+    return typedComponentIDGenerator().apply(Grid.class);
+  }
+
+  static BiFunction<Class, String, String> gridProID() {
+    return typedComponentIDGenerator().apply(GridPro.class);
+  }
+
+  static BiFunction<Class, String, String> buttonID() {
+    return typedComponentIDGenerator().apply(Button.class);
+  }
+
+  static BiFunction<Class, String, String> comboBoxID() {
+    return typedComponentIDGenerator().apply(ComboBox.class);
+  }
+
+  static BiFunction<Class, String, String> datePickerID() {
+    return typedComponentIDGenerator().apply(DatePicker.class);
+  }
+
+  static BiFunction<Class, String, String> labelID() {
+    return typedComponentIDGenerator().apply(Label.class);
+  }
+
+  static BiFunction<Class, String, String> spanID() {
+    return typedComponentIDGenerator().apply(Span.class);
+  }
+
+  static BiFunction<Class, String, String> textfieldID() {
+    return typedComponentIDGenerator().apply(TextField.class);
+  }
+
+  static BiFunction<Class, String, String> passwordID() {
+    return typedComponentIDGenerator().apply(PasswordField.class);
+  }
+
+  static BiFunction<Class, String, String> checkboxID() {
+    return typedComponentIDGenerator().apply(Checkbox.class);
+  }
+
+  static BiFunction<Class, String, String> contextMenuID() {
+    return typedComponentIDGenerator().apply(ContextMenu.class);
+  }
+
+  static BiFunction<Class, String, String> timePickerID() {
+    return typedComponentIDGenerator().apply(TimePicker.class);
+  }
+
+  static BiFunction<Class, String, String> detailsID() {
+    return typedComponentIDGenerator().apply(Details.class);
+  }
+
+  static BiFunction<Class, String, String> dialogID() {
+    return typedComponentIDGenerator().apply(Dialog.class);
+  }
+
+  static BiFunction<Class, String, String> selectID() {
+    return typedComponentIDGenerator().apply(Select.class);
+  }
+
+  static BiFunction<Class, String, String> iconID() {
+    return typedComponentIDGenerator().apply(Icon.class);
+  }
+
+  static BiFunction<Class, String, String> listBoxID() {
+    return typedComponentIDGenerator().apply(ListBox.class);
+  }
+
+  static BiFunction<Class, String, String> progressBarID() {
+    return typedComponentIDGenerator().apply(ProgressBar.class);
+  }
+
+  static BiFunction<Class, String, String> radioBtnGrpID() {
+    return typedComponentIDGenerator().apply(RadioButtonGroup.class);
+  }
+  static BiFunction<Class, String, String> tabID() {
+    return typedComponentIDGenerator().apply(Tab.class);
+  }
+
+  static BiFunction<Class, String, String> uploadID() {
+    return typedComponentIDGenerator().apply(Upload.class);
+  }
+
+  static BiFunction<Class, String, String> customFieldID() {
+    return typedComponentIDGenerator().apply(CustomField.class);
+  }
+
+  static BiFunction<Class, String, String> chartID() {
+    return typedComponentIDGenerator().apply(Chart.class);
+  }
+
+  static BiFunction<Class, String, String> richTextEditorID() {
+    return typedComponentIDGenerator().apply(RichTextEditor.class);
+  }
+
+  static BiFunction<Class, String, String> crudID() {
+    return typedComponentIDGenerator().apply(Crud.class);
+  }
+
+  static BiFunction<Class, String, String> cookieConsentID() {
+    return typedComponentIDGenerator().apply(CookieConsent.class);
+  }
+
+  static BiFunction<Class, String, String> confirmDialogID() {
+    return typedComponentIDGenerator().apply(ConfirmDialog.class);
+  }
+
+
+//  static BiFunction<Class, String, String> itemID() {
+//    return typedComponentIDGenerator().apply(Item.class);
+//  }
+
+//  static BiFunction<Class, String, String> accordeonID() {
+//    return typedComponentIDGenerator().apply(.class);
+//  }
+
+//  Layouts
+
+  static BiFunction<Class, String, String> verticalLayoutID() {
+    return typedComponentIDGenerator().apply(VerticalLayout.class);
+  }
+
+  static BiFunction<Class, String, String> horizontalLayoutID() {
+    return typedComponentIDGenerator().apply(HorizontalLayout.class);
+  }
+
+  static BiFunction<Class, String, String> formLayoutID() {
+    return typedComponentIDGenerator().apply(FormLayout.class);
+  }
+
+  static BiFunction<Class, String, String> splitLayoutID() {
+    return typedComponentIDGenerator().apply(SplitLayout.class);
+  }
+}
